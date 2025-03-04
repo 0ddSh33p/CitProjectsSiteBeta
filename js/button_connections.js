e29d1ad022dacccf8491dcb57bc955d92bafc3ab
Fin <92823911+0ddSh33p@users.noreply.github.com>
2025-03-04 14:22:59 -0500

    Update button_connections.js

--- js/button_connections.js
+++ js/button_connections.js
@@ -6,12 +6,12 @@ async function tryLogin(e){
     // add the final url here
     const baseURL = "https://localhost:3000/tryUser/";
     e.preventDefault();
-    const res = await fetch(baseURL + sendUser.value + "/" + sendPass.value ,
+    const res = await fetch(baseURL + sendUser.value + "/" + sendPass.value,
         {
             method: 'GET'	
         });
     const data = await res.json();
-    console.log(data.propType)
+   //console.log(data.propType)
     if (data.propType == 1){
         window.location.replace("https://localhost:3000/student")
     } else if (data.propType == 2){
@@ -23,7 +23,7 @@ async function tryLogin(e){
 function add_project_cancel() {
     var add_project_box = document.getElementById('add_project_box');
     add_project_box.style.display='none';
-    console.log("add project box is not visible")
+    //console.log("add project box is not visible")
 }
 
 //script to make edit box show
@@ -31,7 +31,7 @@ function add_project_show() {
     var add_project_box = document.getElementById('add_project_box');
     if (add_project_box.style.display ='none') {
         add_project_box.style.display = 'inline-block';
-        console.log("add project box is visible")
+        //console.log("add project box is visible")
     }
 }
 
@@ -40,7 +40,7 @@ function add_project_show() {
 function edit_cancel() {
     var editbox = document.getElementById('edit_box');
     editbox.style.display='none';
-    console.log("editout box is not visible")
+    //console.log("editout box is not visible")
 }
 
 //script to make edit box show
@@ -48,7 +48,7 @@ function edit_show() {
     var editbox = document.getElementById('edit_box');
     if (editbox.style.display ='none') {
         editbox.style.display = 'inline-block';
-        console.log("editout box is visible")
+        //console.log("editout box is visible")
     }
 }
 
@@ -57,7 +57,7 @@ function edit_show() {
 function add_hide() {
     var options = document.getElementById('add-options');
     options.style.display='none';
-    console.log("add box is not visible")
+    //console.log("add box is not visible")
 }
 
 //script to make add options show
@@ -65,7 +65,7 @@ function add_show() {
     var options = document.getElementById('add-options');
     if (options.style.display ='none') {
         options.style.display = 'inline-block';
-        console.log("add box is visible")
+        //console.log("add box is visible")
     }
 }
 
@@ -73,7 +73,7 @@ function add_show() {
 function add_user_hide() {
     var ub = document.getElementById('user_box');
     ub.style.display='none';
-    console.log("add user box is not visible")
+    //console.log("add user box is not visible")
 }
 
 //script to make add options show
@@ -81,7 +81,7 @@ function add_user_show() {
     var ub = document.getElementById('user_box');
     if (ub.style.display ='none') {
         ub.style.display = 'inline-block';
-        console.log("add user box is visible")
+        //console.log("add user box is visible")
     }
 }
 
@@ -90,7 +90,7 @@ function add_user_show() {
 function log_cancel() {
     var logbox = document.getElementById('logout_box');
     logbox.style.display='none';
-    console.log("logout box is not visible")
+    //console.log("logout box is not visible")
 }
 
 //script for making the logout box appear when logout is pressed
@@ -98,7 +98,7 @@ function logout_show() {
     var logbox = document.getElementById('logout_box');
         if (logbox.style.display ='none') {
             logbox.style.display = 'inline-block';
-            console.log("logout box is visible")
+            //console.log("logout box is visible")
         }
 }
 
@@ -114,7 +114,7 @@ async function logout() {
     // After the logout action, redirect to the home page or wherever needed
     window.location.href = "https://localhost:3000"; 
   } catch (error) {
-    console.error("Logout failed:", error);
+    //console.error("Logout failed:", error);
   }
 }
 
@@ -122,7 +122,7 @@ async function logout() {
 function del_cancel() {
     var delbox = document.getElementById('del_box');
     delbox.style.display='none';
-    console.log("delete box is not visible")
+    //console.log("delete box is not visible")
 }
 
 //script for making the logout box appear when logout is pressed
@@ -130,13 +130,13 @@ function del_show() {
     var delbox = document.getElementById('del_box');
         if (delbox.style.display ='none') {
             delbox.style.display = 'inline-block';
-            console.log("delete box is visible")
+            //console.log("delete box is visible")
         }
 }
 
 //Create the project buttons on the admin page
 function add_btn(title){
     buttonZone = document.getElementById('project_buttons');
-
-    buttonZone.innerHTML = buttonZone.innerHTML + '\n <button class="project_tile"> '+title+'</button><br> \n <a href="#" id="edit" onclick="edit_show()">Edit</a><a href="#" onclick="del_show()" class="delete">Delete</a><br>'
+    buttonZone.innerHTML = buttonZone.innerHTML + '\n<div class="admin_button"> \n <button class="project_tile"> '+title+'</button><br> \n <a href="#" id="edit" onclick="edit_show()">Edit</a><a href="#" onclick="del_show()" class="delete">Delete</a><br></div>'
+    add_project_cancel()
 }
