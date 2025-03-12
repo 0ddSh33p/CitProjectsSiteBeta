d6a2329bf2bb23293c70f2de5570c67a391a6197
Fin <92823911+0ddSh33p@users.noreply.github.com>
2025-03-12 13:36:21 -0400

    Update button_connections.js

--- js/button_connections.js
+++ js/button_connections.js
@@ -149,8 +149,9 @@ window.addEventListener("load", async (event) => {
 //Create the project buttons on the admin page
 function add_btn(data){
     buttonZone = document.getElementById('project_buttons');
+    var username = "GET THEIR NAME"
     if(buttonZone != null){
-        buttonZone.innerHTML = buttonZone.innerHTML + '\n<div class="admin_button"> \n <button class="project_tile", onclick = "openVNC('+data.projectname+','+data.username+')"> '+data.projectname+'</button><br> \n <a href="#" onclick="edit_show('+data.id+')">Edit</a><a href="#" onclick="del_show('+data.id+')" class="delete">Delete</a><br></div>'
+        buttonZone.innerHTML = buttonZone.innerHTML + '\n<div class="admin_button"> \n <button class="project_tile", onclick = "openVNC('+data.projectname+','+username+')"> '+data.projectname+'</button><br> \n <a href="#" onclick="edit_show('+data.id+')">Edit</a><a href="#" onclick="del_show('+data.id+')" class="delete">Delete</a><br></div>'
         add_project_cancel()
     }
 }
